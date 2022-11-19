2a96b4aad9e2da31979899ec1d8ac1941f55ae30
rcyrdard <rcyrdard@gmail.com>
2022-11-19 16:06:49 +0100

    ajout d'un fichier Router (avec toutes les routes)

---
+++ kasa/src/Router.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import './styles/Index.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import FicheLogement from './pages/FicheLogement'
+import Error from './pages/Error'
+import Apropos from './pages/Apropos'
+
+
+export default function Routing() {
+    
+    return (
+        <div className='route'>
+            <Router>
+                <Header />
+            <Routes>
+                <Route exact path="/" element={<Home />}/>
+                <Route path="/logements/:id" element={<FicheLogement />}/>
+                <Route path="/Apropos" element={<Apropos/>}/>
+                <Route path="/*" element={<Error/>}/>
+            </Routes>
+                <Footer />
+            </Router>
+        </div>
+    );
+
+}
+
+
--- kasa/src/index.jsx
+++ kasa/src/index.jsx
@@ -1,32 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
-import './styles/Index.css';
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Home from './pages/Home'
-import FicheLogement from './pages/FicheLogement'
-import Error from './pages/Error'
-import Apropos from './pages/Apropos'
-import reportWebVitals from './reportWebVitals';
+import Routing from "./Router.jsx"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<Home />}/>
-        <Route path="/logements/:id" element={<FicheLogement />}/>
-        <Route path="/Apropos" element={<Apropos/>}/>
-        <Route path="/*" element={<Error/>}/>
-      </Routes>
-      <Footer />
-    </Router>
+    <Routing />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
